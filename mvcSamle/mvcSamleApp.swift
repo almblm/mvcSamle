fe10e75e300dd95dcc8879467fa6133202ef6fc1
almblm <sind_blm@yahoo.co.jp>
2021-08-16 22:56:55 +0900

    Initial Commit

---
+++ mvcSamle/mvcSamleApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvcSamleApp.swift
+//  mvcSamle
+//
+//  Created by Ryo Nagasawa on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct mvcSamleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
